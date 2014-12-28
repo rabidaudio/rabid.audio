67ed9ba6554da9ccf2f36613cbe2d405c4c5d5d4
Charles Julian Knight <charles@rabidaudio.com>
2014-12-28 15:17:08 -0500

    design

---
+++ assets/_src/console_message.js
@@ -0,0 +1,5 @@
+console.log('%cWelcome!', 'font-size: large;');
+console.log(
+    '%cBy the way, you can use "n" and "p" keyboard shortcuts to page through posts.',
+    'color: grey; font-style: italic;'
+);
