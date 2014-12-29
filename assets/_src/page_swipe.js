4709065bf55bd1197dc1d560e771cb657109d420
Charles Julian Knight <charles@rabidaudio.com>
2014-12-29 02:35:31 -0500

    workin swipe

---
+++ assets/_src/page_swipe.js
@@ -0,0 +1,31 @@
+$.fn.onSwipe = function(start_callback, stop_callback){
+  $(document)
+    .on('touchstart', function(edown){
+      var touchstart = edown.originalEvent.touches[0];
+      $(document).on('touchend', function(eup){
+        $(this).off('touchend');
+        var touchend = eup.originalEvent.changedTouches[0];
+        var dx = (touchend.pageX - touchstart.pageX)/(eup.originalEvent.timeStamp - edown.originalEvent.timeStamp);
+        var dy = (touchend.pageY - touchstart.pageY)/(eup.originalEvent.timeStamp - edown.originalEvent.timeStamp);
+        stop_callback(dx, dy, edown, eup);
+      });
+    });
+    start_callback(touchstart, edown);
+};
+
+$(document).ready(function(){
+  var THRESHOLD = 0.8;
+  $(document).onSwipe(function(touchstart, edown){
+  },
+  function(dx, dy, edown, eup){
+    if(Math.abs(dx)>THRESHOLD && Math.abs(dx)>Math.abs(dy)){
+      if(dx < 0){
+        //left
+        $('.next a')[0].click();
+      }else{
+        //right
+        $('.prev a')[0].click();
+      }
+    }
+  });
+});
