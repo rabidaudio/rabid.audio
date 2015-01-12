81a626a92a70f9e8871b648e4f9007d824f8e58b
Charles Julian Knight <charles@rabidaudio.com>
2015-01-11 22:06:29 -0500

    dynamic browser-based color coding

--- assets/_src/console_message.js
+++ assets/_src/console_message.js
@@ -1,5 +1,10 @@
-console.log('%cWelcome!', 'font-size: large;');
-console.log(
-    '%cBy the way, you can use "j" and "k" keyboard shortcuts to page through posts, and "g"+"a" to jump to all posts (same as gmail).',
-    'color: grey; font-style: italic;'
-);
+(function(){
+  console.clog = (function(m, s){(navigator.vendor.match(/^Goog/) && s ? this.log('%c'+m, s) : this.log(m));}).bind(console);
+
+  console.clog('Welcome!', 'font-size: large;');
+  console.clog(
+      'By the way, you can use "j" and "k" keyboard shortcuts to page through posts, '+
+      'and "g"+"a" to jump to all posts (same as gmail).',
+      'color: grey; font-style: italic;'
+  );
+})();
