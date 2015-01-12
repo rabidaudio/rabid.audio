28ffcb87d5fda19cd66b413c91082468a3803995
Charles Julian Knight <charles@rabidaudio.com>
2015-01-11 21:35:44 -0500

    fixed scrollTop issue on other platforms

--- assets/_src/dinkus_to_top.js
+++ assets/_src/dinkus_to_top.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){  
-  $('.dinkus a').click(function(e){
-    var dest_pos = $(document.body).offset().top;
-    var distance = dest_pos - $(document).scrollTop();//down is positive
-    e.preventDefault();
-    $(document.body).animate({ scrollTop: dest_pos }, Math.min(2000, Math.abs(distance)) );
+(function($){
+  $(document).ready(function(){  
+    $('.dinkus a').click(function(e){
+      var dest_pos = $(document.body).offset().top;
+      var distance = dest_pos - $(document).scrollTop();//down is positive
+      $('body,html').animate({ scrollTop: dest_pos }, Math.min(2000, Math.abs(distance)) );
+      e.preventDefault();
+    });
   });
-});
+})(jQuery);
