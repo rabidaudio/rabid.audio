0f882a507b8a0ae9bae95a36b8854923b8fb3c7b
Charles Julian Knight <charles@rabidaudio.com>
2022-09-05 18:34:50 -0400

    some theming

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -3,5 +3,3 @@ import "syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
-
-console.info("Bridgetown is loaded!")
