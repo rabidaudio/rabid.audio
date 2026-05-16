2ececfbafa72baac5109dc6893702d595ace3452
Julien <julien@rabidaudio.com>
2026-05-15 20:39:05 -0400

    update image plugin to center default, assume post dir, and support multiple images

--- plugins/builders/post_images.rb
+++ plugins/builders/post_images.rb
@@ -1,4 +1,4 @@
-require 'debug'
+# require 'debug'
 
 class Builders::PostImages < SiteBuilder
     def build
@@ -8,13 +8,33 @@ class Builders::PostImages < SiteBuilder
             args = args.sub(caption_arg, '') if caption_arg
             classes = args.split(' ')
             css_class = ['image-container', *classes].join(' ')
-            images = tag.content.lines.map(&:chomp).reject(&:empty?).map do |l|
-                url = l.starts_with?("http") ? l : "/images/#{l}"
-                "<img src='#{url}'#{caption ? " alt=\"#{caption}\"" : ""}>"
+            images = tag.content.lines.map(&:chomp).reject(&:empty?).map(&:strip).map do |l|
+                url = expand_src(l, tag.context)
+                alt = caption ? " alt=\"#{caption}\"" : ""
+                <<~HTML
+                    <div class="#{css_class}">
+                        <img src='#{url}'#{alt}>
+                    </div>
+                HTML
             end.join("\n")
             <<~HTML
-                <div class="#{css_class}">#{images}</div>
+                <div class="image-parent">#{images}</div>
             HTML
         end
+    end
+    
+    private
+
+    def expand_src(src, context)
+        return src if src.starts_with?("http")
+
+        # check for the image in the post-specific image folder first
+        page_name = File.basename(context['page']['relative_path'], ".*")
+        absolute = File.join(context['site']['source'], 'images', page_name, src)
+        # debugger
+        return File.join('/images', page_name, src) if File.exist?(absolute)
+        
+        # if not there, assume it's relative to the top-level image folder
+        File.join('/images', src)
     end
 end
